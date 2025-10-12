35d741784df40a98b022213661a2b3acdb5aba50
simple github test <reynardo@timba.co>
2025-10-12 01:49:31 -0400

    minor cleanup

--- Engine/IO/AssetBuilders/TextureAssetBuilder.cs
+++ Engine/IO/AssetBuilders/TextureAssetBuilder.cs
@@ -10,17 +10,6 @@ namespace Engine.IO
 {
     internal class TextureAssetBuilder : AssetBuilderBase
     {
-        //internal override async Task<EObject> BuildAsset(AssetInfo info, Guid guid, BinaryReader reader)
-        //{
-        //    var width = reader.ReadInt32();
-        //    var height = reader.ReadInt32();
-        //    var comp = reader.ReadInt32();
-
-        //    var imageData = reader.ReadBytes(width * height * comp);
-
-        //    return new Texture2D(width, height, comp, imageData);
-        //}
-
         internal override EObject BuildAsset(AssetInfo info, Guid guid, BinaryReader reader)
         {
             var width = reader.ReadInt32();
